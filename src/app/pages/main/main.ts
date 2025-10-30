b607a909d2385e26f071c33cf11f4e70e7fc0ef0
diegor <diegorutti26@gmail.com>
2025-10-30 15:01:28 -0500

    first commit

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
-export class Main {
+export class Main implements OnInit, OnDestroy {
+  
+  images: string[] = [
+    'assets/imga.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg'
+  ];
 
+  currentIndex = 0;
+  interval: any;
+
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
 }
